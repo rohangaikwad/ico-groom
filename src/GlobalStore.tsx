434ff9c60c35a8b9246e4b37e8398ca6f38de3e1
Manish Mistry <manish.mistry@hotmail.com>
2022-12-17 11:59:00 +0530

    Fixed Icon Selection store issue

--- src/GlobalStore.tsx
+++ src/GlobalStore.tsx
@@ -15,7 +15,6 @@ const Store = () => {
   const [activeCategory, setActiveCategory] = createSignal<string>("solid6");
   const [searchTerm, setSearchTerm] = createSignal<string>("");
 
-
   const fa47 = Fa47 as IFontDefType;
   const solid6 = FaSolid6 as IFontDefType;
   const regular6 = FaRegular6 as IFontDefType;
@@ -33,8 +32,16 @@ const Store = () => {
     thin6: [],
     brands6: [],
     duotone6: [],
-    fa47: fa47.icons.filter(icon => [61442, 61443, 61444, 61445, 61446, 61452, 61453, 61459, 61523, 61524, 61543, 61544, 61562, 61559, 61560, 61578, 61579, 61580, 61569, 61570, 61584, 61589, 61594, 61596, 61664, 61700, 61701, 61702, 61703, 61798, 61802, 62144, 61550, 61555, 61505, 61487, 61449, 61450, 61451, 61475, 61504, 61508, 61593, 61616, 61944, 62000].includes(icon.defaultCode))
-  }, {equals: false});
+    fa47: fa47.icons.filter((icon) =>
+      [
+        61442, 61443, 61444, 61445, 61446, 61452, 61453, 61459, 61523, 61524,
+        61543, 61544, 61562, 61559, 61560, 61578, 61579, 61580, 61569, 61570,
+        61584, 61589, 61594, 61596, 61664, 61700, 61701, 61702, 61703, 61798,
+        61802, 62144, 61550, 61555, 61505, 61487, 61449, 61450, 61451, 61475,
+        61504, 61508, 61593, 61616, 61944, 62000,
+      ].includes(icon.defaultCode)
+    ),
+  });
 
   return {
     activeCategory,
@@ -51,8 +58,8 @@ const Store = () => {
     selected,
     setSelected,
     searchTerm,
-    setSearchTerm
+    setSearchTerm,
   };
-}
+};
 
 export default createRoot(Store);
--- src/components/Select/FontCategory.tsx
+++ src/components/Select/FontCategory.tsx
@@ -5,115 +5,153 @@ import Store from "./../../GlobalStore";
 
 import { IIconType, IFontDefType, ISelectedType } from "../../types/Interfaces";
 
-interface IVirtualItem {  
-  key: string | number
-  index: number
-  start: number
-  end: number
-  size: number
+interface IVirtualItem {
+  key: string | number;
+  index: number;
+  start: number;
+  end: number;
+  size: number;
 }
 
 const FontCategory: Component<{
-  name: string,
-  category: IFontDefType
-}> = ({name, category}) => {
-
+  name: string;
+  category: IFontDefType;
+}> = ({ name, category }) => {
   let scrollParentRef: HTMLDivElement | undefined;
   const { icons } = category;
   const [fIcons, setFIcons] = createSignal(category.icons);
-  const categoryName = category.metadata.name.replace('fa-', '') as keyof ISelectedType;
+  const categoryName = category.metadata.name.replace(
+    "fa-",
+    ""
+  ) as keyof ISelectedType;
 
   const { selected, setSelected, searchTerm } = Store;
 
   const [rowWidth, setRowWidth] = createSignal(1110);
-  const [iconSize,] = createSignal(40);
-  const [itemsPerRow, setItemsPerRow] = createSignal(Math.floor(rowWidth() / ((iconSize() * 3) + 10)));
+  const [iconSize] = createSignal(40);
+  const [itemsPerRow, setItemsPerRow] = createSignal(
+    Math.floor(rowWidth() / (iconSize() * 3 + 10))
+  );
 
   const rowVirtualizer = createVirtualizer({
     count: category.icons.length / itemsPerRow(),
     getScrollElement: () => scrollParentRef,
     estimateSize: () => 73,
   });
-  console.log(rowVirtualizer)
+  console.log(rowVirtualizer);
 
   window.addEventListener("resize", () => {
-    let width = document.querySelector(".icon-list-scroll > .icon-list")?.clientWidth || 300;
+    let width =
+      document.querySelector(".icon-list-scroll > .icon-list")?.clientWidth ||
+      300;
     setRowWidth(width);
   });
 
   createEffect(() => {
     if (searchTerm().length > 2) {
-      let _icons = icons.filter((icon) => icon.tags[0].indexOf(searchTerm()) > -1);
+      let _icons = icons.filter(
+        (icon) => icon.tags[0].indexOf(searchTerm()) > -1
+      );
       if (_icons.length > 0) {
-        rowVirtualizer.scrollToOffset({toOffset: 0})
+        rowVirtualizer.scrollToOffset({ toOffset: 0 });
         setFIcons(_icons);
       } else {
-        rowVirtualizer.scrollToOffset({toOffset: 0})
+        rowVirtualizer.scrollToOffset({ toOffset: 0 });
         setFIcons(icons);
       }
-
     } else if (icons.length !== fIcons.length) {
       setFIcons(icons);
     }
   });
 
   createEffect(() => {
-    let _iconSize = (iconSize() * 3) + 10;
+    let _iconSize = iconSize() * 3 + 10;
     setItemsPerRow(Math.floor(rowWidth() / _iconSize));
-  })
-
+  });
 
   const toggleSelection = (icon: IIconType) => {
-    let _selected = selected();
-    let index = _selected[categoryName].findIndex((s) => s.defaultCode === icon.defaultCode);
+    setSelected((prev) => {
+      let _selected = Object.assign({}, prev);
 
-    if (index > -1) {
-      _selected[categoryName].splice(index, 1);
-    } else {
-      _selected[categoryName].push(icon);
-    }
+      let index = _selected[categoryName]?.findIndex(
+        (s) => s.defaultCode === icon.defaultCode
+      );
 
-    setSelected(_selected);
-  }
+      if (index > -1) {
+        _selected[categoryName].splice(index, 1);
+      } else {
+        _selected[categoryName].push(icon);
+      }
+
+      return _selected;
+    });
+  };
 
   const IsSelected = (icon: IIconType) => {
     let _selected = selected();
-    let index = _selected[categoryName].findIndex((s) => s.defaultCode === icon.defaultCode);
+    let index = _selected[categoryName].findIndex(
+      (s) => s.defaultCode === icon.defaultCode
+    );
     return index > -1;
-  }
-
+  };
 
   return (
     <>
-      {fIcons().length > 0 && <div class={`${name.toLowerCase()} icons`}>
-        <div class="name">
-          <h3>{name}</h3>
-        </div>
-        
-        <div ref={scrollParentRef} class="icon-list-scroll">          
-          <For each={rowVirtualizer.getVirtualItems()}>
-            {(virtualItem: IVirtualItem) => {
-              return <ul class="icon-list"
-                style={{
-                  position: 'absolute',
-                  top: 0,
-                  left: 0,
-                  width: '100%',
-                  height: `${virtualItem.size}px`,
-                  transform: `translateY(${virtualItem.start}px)`,
-                }}
-              >
-                {fIcons().slice(virtualItem.index * itemsPerRow(), (virtualItem.index + 1) * itemsPerRow()).map((icon) => <li data-code={icon?.defaultCode} onClick={() => toggleSelection(icon)} class={IsSelected(icon) ? "active" : ""} >
-                  <svg width={32} height={32} viewBox={`0 0 ${icon.width || 1024} 1024`}>
-                    {icon.paths.map((path) => <path d={path} /> )}
-                  </svg>
-                  <span>{icon?.tags[0] || ""} - {selected()[categoryName].findIndex((s) => s.defaultCode === icon.defaultCode)}</span>
-                </li>)}
-              </ul>
-            }}
-          </For>
-        </div>
-        {/* <ul class="icon-list">
+      {fIcons().length > 0 && (
+        <div class={`${name.toLowerCase()} icons`}>
+          <div class="name">
+            <h3>{name}</h3>
+          </div>
+
+          <div ref={scrollParentRef} class="icon-list-scroll">
+            <For each={rowVirtualizer.getVirtualItems()}>
+              {(virtualItem: IVirtualItem) => {
+                return (
+                  <ul
+                    class="icon-list"
+                    style={{
+                      position: "absolute",
+                      top: 0,
+                      left: 0,
+                      width: "100%",
+                      height: `${virtualItem.size}px`,
+                      transform: `translateY(${virtualItem.start}px)`,
+                    }}
+                  >
+                    {fIcons()
+                      .slice(
+                        virtualItem.index * itemsPerRow(),
+                        (virtualItem.index + 1) * itemsPerRow()
+                      )
+                      .map((icon) => (
+                        <li
+                          data-code={icon?.defaultCode}
+                          onClick={() => toggleSelection(icon)}
+                          class={IsSelected(icon) ? "active" : ""}
+                        >
+                          <svg
+                            width={32}
+                            height={32}
+                            viewBox={`0 0 ${icon.width || 1024} 1024`}
+                          >
+                            {icon.paths.map((path) => (
+                              <path d={path} />
+                            ))}
+                          </svg>
+                          <span>
+                            {icon?.tags[0] || ""} -{" "}
+                            {selected()[categoryName].findIndex(
+                              (s) => s.defaultCode === icon.defaultCode
+                            )}
+                          </span>
+                        </li>
+                      ))}
+                  </ul>
+                );
+              }}
+            </For>
+          </div>
+          {/* <ul class="icon-list">
           {fIcons().slice(0, limit()).map((icon) => <li data-code={icon?.defaultCode} onClick={() => toggleSelection(icon)} class={IsSelected(icon) ? "active" : ""} >
             <svg width={32} height={32} viewBox={`0 0 ${icon.width || 1024} 1024`}>
               {icon.paths.map((path) => <path d={path} /> )}
@@ -121,7 +159,8 @@ const FontCategory: Component<{
             <span>{icon?.tags[0] || ""}</span>
           </li>)}
         </ul> */}
-      </div>}
+        </div>
+      )}
     </>
   );
 };
